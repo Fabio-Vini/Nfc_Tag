d112acd476c20e6191e74a0fa24c762a5dbe3735
Fabio-Vini <88862357+Fabio-Vini@users.noreply.github.com>
2024-01-07 21:33:07 -0300

    Add files via upload

---
+++ NFC_EXCEL/NFC/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace NFC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TAG:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(118, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(519, 30);
+            this.textBox1.TabIndex = 1;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 125);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ NFC_EXCEL/NFC/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace NFC
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort serialPort;
+        private Excel.Application excelApp;
+        private Excel.Workbook workbook;
+        private Excel.Worksheet worksheet;
+        private int rowNum = 1;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeSerialPort();
+            InitializeExcel();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InitializeExcel()
+        {
+            try 
+            {
+                //Inicia o Excel
+                excelApp = new Excel.Application();
+                excelApp.Visible = true;
+
+                //Adiciona um novo workbook e worksheet 
+                workbook =  excelApp.Workbooks.Add();
+                worksheet = (Excel.Worksheet)workbook.Worksheets[1];
+
+                //Adiciona cabe~çalhos á primeira linha da planilha 
+                worksheet.Cells[1, 1] = "Tag RFID";
+            }
+            catch(Exception ex) 
+            {
+                MessageBox.Show($"Erro ao inicializar o Excel: {ex.Message}");
+            }
+        }
+
+        private void InitializeSerialPort()
+        {
+            serialPort = new SerialPort("COM4", 9600); // Substitua "COMx" pela porta COM do seu Arduino
+            serialPort.DataReceived += SerialPort_DataReceived;
+
+            try
+            {
+                serialPort.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao abrir a porta serial: {ex.Message}");
+            }
+        }
+
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                try
+                {
+                    string dataReceived = serialPort.ReadLine();
+                    Invoke(new Action(() =>
+                    {
+                        textBox1.AppendText(dataReceived + Environment.NewLine);
+                        //Adiciona os dados ao Excel
+                        worksheet.Cells[++rowNum, 1] = dataReceived;
+                    }));
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Erro ao ler dados: {ex.Message}");
+                }
+
+                try
+                {
+                    string dataReceived = serialPort.ReadLine();
+                    // Atualize a interface gráfica com os dados recebidos
+                    Invoke(new Action(() => textBox1.AppendText(dataReceived + Environment.NewLine))); 
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Erro ao ler dados: {ex.Message}");
+                }
+            }
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                serialPort.Close();
+            }
+
+            //Salva e fecha o workbook ao fechar o formulário 
+            if(workbook != null) 
+            {
+                workbook.SaveAs("Caminho\\Para\\O\\Arquivo\\Excel.xlsx"); //Dentro dos parenteses voce cola o caminho onde é pra salvar o excel, o exemplo ta escrito ali dentr    o 
+                workbook.Close();
+            }
+
+            excelApp.Quit();
+        }
+    }
+}
